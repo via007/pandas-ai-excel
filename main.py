5541ce8845af39de6543a1cf963a573c86370042
via <he_via@163.com>
2025-03-22 10:59:01 +0800

    生成的图表适配中文字体

--- main.py
+++ main.py
@@ -5,14 +5,15 @@ import pandasai as pai
 import matplotlib.pyplot as plt
 from pandasai_openai import OpenAI
 
-plt.rcParams['font.sans-serif'] = ['SimHei']
+plt.rcParams['font.sans-serif'] = ['Hiragino Sans GB']  # 使用 Mac OS 自带的苹方字体
+plt.rcParams['axes.unicode_minus'] = False
 
 os.makedirs("./exports/temp", exist_ok=True)
 api_token = os.getenv('OPENAI_KEY')
 
 
-llm = OpenAI(api_token=api_token, api_base="https://dashscope.aliyuncs.com/compatible-mode/v1")
-llm.model = 'deepseek-v3'  # qwen-max-0125 deepseek-v3
+llm = OpenAI(api_token="sk-2080251fa71e4c5991ad8836b35b0c00", api_base="https://dashscope.aliyuncs.com/compatible-mode/v1")
+llm.model = 'qwen-max-0125'  # qwen-max-0125 deepseek-v3
 pai.config.set({"llm": llm})
 
 
@@ -46,5 +47,6 @@ def process_file_and_question(file_path, question):
         else:
             return response.value, None, None
 
-    except:
+    except Exception as e:
+        print(e)
         return f"系统异常，请重试...", None, None
